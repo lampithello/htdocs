e5def5375836394f063eb5aff4fa196198fabf1b
gabrielefasanoo <gabrielefasano13@gmail.com>
2024-06-16 21:46:34 +0200

    refactor: Update database connection credentials in conn.php

--- control/conn.php
+++ control/conn.php
@@ -1,7 +1,15 @@
 <?php
-$conn = new mysqli("localhost", "root", "hcKnOWk9VMHT9WSk", "test");
+$config = require 'config.php';
+
+$servername = $config['servername'];
+$username = $config['username'];
+$password = $config['password'];
+$dbname = $config['dbname'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_errno) {
     printf("<h1>Connessione al server Mysql fallita: %s</h1>", $conn->connect_error);
     exit();
 }
-
+?>
