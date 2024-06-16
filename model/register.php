c4cdb1d345f9e839686125fafd644d82144823c6
gabrielefasanoo <gabrielefasano13@gmail.com>
2024-06-16 13:09:14 +0200

    chore: Refactor register.php functions and update naming conventions

--- model/register.php
+++ model/register.php
@@ -2,43 +2,42 @@
 
 include_once '../control/conn.php';
 
-function password_criptata($password) {
+function passwordCriptata($password) {
     return password_hash($password, PASSWORD_DEFAULT);
 }
 
-function query_insert($conn, $name, $surname, $gender, $email, $password, $role) {
+function queryInsert($conn, $name, $surname, $gender, $email, $password, $role) {
+    $result = false;  // Variabile per tenere traccia del risultato
     $sql = "INSERT INTO users (name, surname, gender, email, password, role) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
+    
     if (!$stmt) {
         echo "Errore nella preparazione della query: " . $conn->error;
-        return false;
-    }
-
-    $stmt->bind_param("ssssss", $name, $surname, $gender, $email, $password, $role);
-    if (!$stmt->execute()) {
-        echo "Errore nell'esecuzione della query: " . $stmt->error;
-        return false;
-    }
-
-    if ($stmt->affected_rows > 0) {
-        return true;
     } else {
-        echo "Error: unable to insert data";
-        return false;
+        $stmt->bind_param("ssssss", $name, $surname, $gender, $email, $password, $role);
+        if ($stmt->execute()) {
+            if ($stmt->affected_rows > 0) {
+                $result = true;
+            } else {
+                echo "Error: unable to insert data";
+            }
+        } else {
+            echo "Errore nell'esecuzione della query: " . $stmt->error;
+        }
     }
+    
+    return $result;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // var_dump($_POST); // Verifica se il form è stato inviato correttamente
-
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $gender = $_POST['gender'];
     $email = $_POST['email'];
-    $password = password_criptata($_POST['password']); // Hash della password
+    $password = passwordCriptata($_POST['password']); // Hash della password
     $role = $_POST['role'];
 
-    if (query_insert($conn, $name, $surname, $gender, $email, $password, $role)) {
+    if (queryInsert($conn, $name, $surname, $gender, $email, $password, $role)) {
         echo "Registration successful";
         header("Location: ../view/index.php");
         exit();
@@ -46,3 +45,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 $conn->close();
+
